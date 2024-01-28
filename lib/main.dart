b143d428b896dd6586d6d1b9b4e38b89042232df
Aymen <bestaouiaymene@gmail.com>
2024-01-28 17:41:35 +0100

    - just a fake push to origin development

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Flutter Demos',
       theme: ThemeData(
         // This is the theme of your application.
         //
