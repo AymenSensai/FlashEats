35f5dcea821e59630c125d83ba95f2165df5fd39
Aymen <bestaouiaymene@gmail.com>
2024-02-02 17:53:45 +0100

    - Refactor LoginScreen

--- lib/features/login/ui/login_screen.dart
+++ lib/features/login/ui/login_screen.dart
@@ -2,7 +2,7 @@ import 'package:flash_eats/core/helpers/spacing.dart';
 import 'package:flash_eats/core/widgets/app_text_button.dart';
 import 'package:flash_eats/features/login/data/models/login_request_body.dart';
 import 'package:flash_eats/features/login/logic/cubit/login_cubit.dart';
-import 'package:flash_eats/features/login/ui/widgets/already_have_account_text.dart';
+import 'package:flash_eats/features/login/ui/widgets/dont_have_account_text.dart';
 import 'package:flash_eats/features/login/ui/widgets/email_and_password.dart';
 import 'package:flash_eats/features/login/ui/widgets/login_bloc_listener.dart';
 import 'package:flash_eats/features/login/ui/widgets/or_sign_in_with.dart';
@@ -79,12 +79,7 @@ class _LoginScreenState extends State<LoginScreen> {
 
   void validateThenDoLogin(BuildContext context) {
     if (context.read<LoginCubit>().formKey.currentState!.validate()) {
-      context.read<LoginCubit>().emitLoginStates(
-            LoginRequestBody(
-              email: context.read<LoginCubit>().emailController.text,
-              password: context.read<LoginCubit>().passwordController.text,
-            ),
-          );
+      context.read<LoginCubit>().emitLoginStates();
     }
   }
 }
